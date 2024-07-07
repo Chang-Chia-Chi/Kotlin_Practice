45e1969ca762c17dbef94b631de19233de71ede8
Max Chang <maxchang0724@gmail.com>
2024-07-07 21:43:42 +0800

    test: fix test

--- Scheduler/src/test/kotlin/client/MQClientTest.kt
+++ Scheduler/src/test/kotlin/client/MQClientTest.kt
@@ -3,36 +3,26 @@ package client
 import org.example.mockConstructor.config.client.MQClient
 import org.example.mockConstructor.config.client.MQContext
 import org.example.mockConstructor.config.config.MQConfig
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.MockedConstruction
-import org.mockito.Mockito
+import org.mockito.Mock
 import org.mockito.Mockito.times
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.verify
 
 class MQClientTest {
+    @Mock
+    lateinit var context: MQContext
+
     lateinit var mQConfig: MQConfig
     lateinit var client: MQClient
-    lateinit var context: MQContext
-    lateinit var mockConstructor: MockedConstruction<MQContext>
 
     @BeforeEach
     fun setUp() {
         MockitoAnnotations.openMocks(this)
-        mockConstructor =
-            Mockito.mockConstruction(MQContext::class.java) { mocked, ctx ->
-                context = mocked
-            }
         mQConfig = MQConfig(event = "test_event")
         client = MQClient(mQConfig, context)
-    }
-
-    @AfterEach
-    fun tearDown() {
-        mockConstructor.close()
     }
 
     @Test
