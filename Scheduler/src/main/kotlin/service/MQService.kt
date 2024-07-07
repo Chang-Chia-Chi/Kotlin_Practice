dadd3a9ed35571034bd031ea321b826ee306c078
Max Chang <maxchang0724@gmail.com>
2024-07-07 21:58:10 +0800

    refactor: no need of open

--- Scheduler/src/main/kotlin/service/MQService.kt
+++ Scheduler/src/main/kotlin/service/MQService.kt
@@ -2,8 +2,8 @@ package org.example.mockConstructor.config.service
 
 import org.example.mockConstructor.config.client.MQClient
 
-open class MQService(
-    open val client: MQClient,
+class MQService(
+    val client: MQClient,
 ) {
     fun sendMessage(msg: String) = client.sendMessage(msg)
 
